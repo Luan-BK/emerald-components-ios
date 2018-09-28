e09167a64728c5855e8a1db2235828f4f02e52e3
Unknown <joaovictor_mendes@hotmail.com>
2018-09-28 17:04:38 -0300

    :construction: Applies new changes to sales simulator

--- EmeraldComponentsApp/EmeraldComponentsApp/MainViewController.swift
+++ EmeraldComponentsApp/EmeraldComponentsApp/MainViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import EmeraldComponents
 
 class MainViewController: UITableViewController {
 
@@ -19,5 +20,73 @@ class MainViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: - Table view delegate and data source
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.section {
+        case 1:
+            switch indexPath.row {
+            case 1:
+                self.pushViewController()
+                
+            case 2:
+                self.pushViewController2()
+            default:
+                break
+            }
+        default:
+            break
+        }
+    }
+    
+    func pushViewController() {
+        let identifier = String(describing: SalesSimulationTableViewController.self)
+        let storyboard = UIStoryboard(name: "SalesSimulation", bundle: Bundle.basic)
+        if let vc = storyboard.instantiateViewController(withIdentifier: identifier) as? SalesSimulationTableViewController {
+            vc.delegate = self
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func pushViewController2() {
+        let identifier = String(describing: SalesSimulatedTableViewController.self)
+        let storyboard = UIStoryboard(name: "SaleSimulated", bundle: Bundle.basic)
+        if let vc = storyboard.instantiateViewController(withIdentifier: identifier) as? SalesSimulatedTableViewController {
+            vc.delegate = self
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 
+}
+
+extension MainViewController: SaleSimulationDelegate {
+    
+    func didSelectSaleSimulation() -> SalesSimulationViewModel {
+        let model = SalesSimulationModel(brands: ["Visa", "Master", "Stone"], fees: ["A vista", "Parcelado"])
+        return SalesSimulationViewModel(model: model)
+    }
+    
+    func didPushViewController(origin: UIViewController) {
+        let identifier = String(describing: SalesSimulatedTableViewController.self)
+        let storyboard = UIStoryboard(name: "SaleSimulated", bundle: Bundle.basic)
+        if let vc = storyboard.instantiateViewController(withIdentifier: identifier) as? SalesSimulatedTableViewController {
+            vc.delegate = self
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+
+}
+
+extension MainViewController: SalesSimulatedDelegate {
+    
+    func retrieveViewModel() -> SalesSimulatedViewModel {
+        let simulated = SaleSimulatedModel()
+        simulated.grossAmount = 100.0
+        simulated.prepayAmount = 10
+        simulated.totalNetAmount = simulated.grossAmount - simulated.prepayAmount
+        
+        return SalesSimulatedViewModel(model: simulated)
+    }
+    
 }
