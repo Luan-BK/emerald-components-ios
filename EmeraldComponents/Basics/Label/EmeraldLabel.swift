832646a0ada737adc7319d2869df534d5e444f7d
Victor Mendes <joaovictor_mendes@hotmail.com>
2018-10-23 15:38:15 -0300

    :heavy_plus_sign: Creates new label component

--- EmeraldComponents/Basics/Label/EmeraldLabel.swift
+++ EmeraldComponents/Basics/Label/EmeraldLabel.swift
@@ -11,8 +11,8 @@ import UIKit
 public enum EmeraldLabelStyle: Int {
     case fill = 1
     case outline
-    case ontyText
-    case textWithImage
+    case text
+    case image
 }
 
 public enum EmeraldLabelState: Int {
@@ -24,16 +24,22 @@ public enum EmeraldLabelState: Int {
 }
 
 @IBDesignable
-public class EmeraldLabel: UILabel {
+public class EmeraldLabel: UIView {
     
     // MARK: - Properties
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    internal var iconLabel: String = "calendar-icon"
     
     internal private(set) var style: EmeraldLabelStyle = .fill {
         didSet {
             self.typeOrStyleDidChange(self.type, self.style)
         }
     }
-    
+
     internal private(set) var type: EmeraldLabelState = .error {
         didSet {
             self.typeOrStyleDidChange(self.type, self.style)
@@ -42,93 +48,129 @@ public class EmeraldLabel: UILabel {
     
     // MARK: - Inspectables
     
-    @IBInspectable var borderRadius: CGFloat = 10.0 {
+    @IBInspectable public var text: String = "" {
         didSet {
-            self.layer.cornerRadius = borderRadius
-        }
-    }
-    
-    @IBInspectable var borderWidth: CGFloat = 1.0 {
-        didSet {
-            self.layer.borderWidth = borderWidth
-        }
-    }
-    
-    @IBInspectable var styleAdapter: Int {
-        get {
-            return self.style.rawValue
-        }
-        set {
-            self.style = EmeraldLabelStyle(rawValue: newValue) ?? .fill
-        }
-    }
-    
-    @IBInspectable var stateAdapter: Int {
-        get {
-            return self.type.rawValue
-        }
-        set {
-            self.type = EmeraldLabelState(rawValue: newValue) ?? .error
+            self.textLabel.text = text.uppercased()
         }
     }
     
     // MARK: - Init
-    
-    public override init(frame: CGRect) {
+
+    override init(frame: CGRect) {
         super.init(frame: frame)
-        self.commonInit()
+        self.xibSetup()
     }
     
-    required public init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        self.commonInit()
+        self.xibSetup()
     }
     
-    private func commonInit() {
-        self.layer.cornerRadius = 15.0
-        self.layer.masksToBounds = true
-        self.textAlignment = .center
+    private func xibSetup() {
+        Bundle.basic.loadNibNamed(String(describing: EmeraldLabel.self), owner: self, options: nil)
         
-        self.font = UIFont.systemFont(ofSize: 18.0, weight: .medium)
+        addSubview(self.contentView)
+        
+        self.contentView.frame = self.bounds
+        self.contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        setDefaultLabel()
+    }
+    
+    internal func setDefaultLabel() {
+        self.textLabel.layer.cornerRadius = 15.0
+        self.textLabel.layer.masksToBounds = true
+        self.textLabel.textAlignment = .center
+        self.textLabel.font = UIFont.systemFont(ofSize: 18.0, weight: .medium)
     }
     
     // MARK: - Public configuration
     
-    public func setImage(_ name: String? = "button-add-icon") {
-        let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
-//        imageView.frame = CGRect(x: 0, y: 0, width: 60, height: 60)
-        
-        let image = UIImage(named: name!)
-        imageView.image = image
-        
-
-//        var imageView = UIImageView(frame: CGRect(100, 150, 150, 150)); // set as you want
-//        var image = UIImage(named: name!);
-//        imageView.image = image;
-//        self.view.addSubview(imageView);
+    public func setIcon(name: String) {
+        iconLabel = name
+    }
+    
+    public func setLabel(text: String) {
+        self.textLabel.text = text.uppercased()
+    }
+    
+    public func configure(style: EmeraldLabelStyle? = nil, type: EmeraldLabelState? = nil) {
+        self.style = style ?? self.style
+        self.type = type ?? self.type
     }
     
     // MARK: - State changes
-    
+
     internal func typeOrStyleDidChange(_ newType: EmeraldLabelState, _ newStyle: EmeraldLabelStyle) {
-        self.backgroundColor = self.backgroundColor(forType: newType, andStyle: newStyle)
-        self.textColor = textColor(forType: newType, andStyle: newStyle)
-        self.tintColor = self.textColor(forType: newType, andStyle: newStyle)
+        self.textLabel.backgroundColor = self.backgroundColor(forType: newType, andStyle: newStyle)
+        self.textLabel.textColor = textColor(forType: newType, andStyle: newStyle)
+        self.textLabel.tintColor = self.textColor(forType: newType, andStyle: newStyle)
+        self.textLabel.layer.borderColor = self.borderColor(forType: newType, andStyle: newStyle)
+        self.textLabel.layer.borderWidth = self.borderWidth(forType: newType, andStyle: newStyle)
+        
+        self.iconImage.image = self.textBullet(forType: newType, andStyle: newStyle)
+        self.iconImage.tintColor = self.textColor(forType: newType, andStyle: newStyle)
     }
-    
+
     // MARK: - Aux
-    
+
     internal func backgroundColor(forType type: EmeraldLabelState, andStyle style: EmeraldLabelStyle) -> UIColor {
         switch style {
         case .fill:
             switch type {
-            case .error: return UIColor.Palette.Basic.error
-            case .warning: return UIColor.Palette.Basic.warning
-            case .neutral: return UIColor.Palette.Light.white4
-            case .success: return UIColor.Palette.Basic.success
-            case .info: return UIColor.Palette.purple
+            case .error:
+                return UIColor.Palette.Basic.error
+            case .warning:
+                return UIColor.Palette.Basic.warning
+            case .neutral:
+                return UIColor.Palette.Light.white4
+            case .success:
+                return UIColor.Palette.Basic.success
+            case .info:
+                return UIColor.Palette.purple
             }
-        default: return UIColor.clear
+        default:
+            return UIColor.clear
+        }
+    }
+    
+    internal func borderWidth(forType type: EmeraldLabelState, andStyle style: EmeraldLabelStyle) -> CGFloat {
+        switch style {
+        case .outline:
+            return 1.0
+        default:
+            return 0.0
+        }
+    }
+    
+    internal func borderColor(forType type: EmeraldLabelState, andStyle style: EmeraldLabelStyle) -> CGColor {
+        switch style {
+        case .fill, .outline:
+            switch type {
+            case .error:
+                return UIColor.Palette.Basic.error.cgColor
+            case .warning:
+                return UIColor.Palette.Basic.warning.cgColor
+            case .neutral:
+                return UIColor.Palette.Light.white4.cgColor
+            case .success:
+                return UIColor.Palette.Basic.success.cgColor
+            case .info:
+                return UIColor.Palette.purple.cgColor
+            }
+        default:
+            return UIColor.clear.cgColor
+        }
+    }
+    
+    internal func textBullet(forType type: EmeraldLabelState, andStyle style: EmeraldLabelStyle) -> UIImage? {
+        switch style {
+        case .text:
+            return UIImage(named: "calendar-icon", in: Bundle.basic, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+        case .image:
+            return UIImage(named: iconLabel, in: Bundle.basic, compatibleWith: nil)!.withRenderingMode(.alwaysTemplate)
+        default:
+            return nil
         }
     }
     
@@ -136,13 +178,18 @@ public class EmeraldLabel: UILabel {
         switch style {
         case .fill:
             return UIColor.Palette.Light.white1
-        case .outline, .ontyText, .textWithImage:
+        case .outline, .text, .image:
             switch type {
-            case .error: return UIColor.Palette.Basic.error
-            case .warning: return UIColor.Palette.Basic.warning
-            case .neutral: return UIColor.Palette.Light.white4
-            case .success: return UIColor.Palette.Basic.success
-            case .info: return UIColor.Palette.purple
+            case .error:
+                return UIColor.Palette.Basic.error
+            case .warning:
+                return UIColor.Palette.Basic.warning
+            case .neutral:
+                return UIColor.Palette.Light.white4
+            case .success:
+                return UIColor.Palette.Basic.success
+            case .info:
+                return UIColor.Palette.purple
             }
         }
     }
