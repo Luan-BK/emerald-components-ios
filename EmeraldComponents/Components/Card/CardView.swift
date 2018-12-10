36c90731c79585c2f6f01b7ef8fe84395ff0b529
Unknown <joaovictor_mendes@hotmail.com>
2018-12-10 17:36:54 -0200

    :art: Adds constraints to subview

--- EmeraldComponents/Components/Card/CardView.swift
+++ EmeraldComponents/Components/Card/CardView.swift
@@ -37,21 +37,59 @@ public class CardView: UICollectionViewCell {
         
         self.content.frame = self.bounds
         self.content.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-        
-        self.setDefaultValues()
     }
     
-    internal func setDefaultValues() {
+    public override func layoutSubviews() {
+        self.customView.clipsToBounds = true
+        self.customView.backgroundColor = UIColor.Palette.Light.white1
         self.customView.layer.cornerRadius = 20
         self.customView.layer.shadowColor = UIColor.gray.cgColor
         self.customView.layer.shadowOpacity = 0.18
         self.customView.layer.shadowOffset = CGSize.zero
         self.customView.layer.shadowRadius = 12.0
-        self.customView.backgroundColor = UIColor.Palette.Light.white1
     }
     
-    public func addCustomSubview(view: UIView) {
+    public func addCustomSubview(view: UIView, width: CGFloat, height: CGFloat) {
         self.customView.addSubview(view)
+        self.constraint(view: view, width: width, height: height)
     }
-
+    
+    internal func constraint(view: UIView, width: CGFloat, height: CGFloat) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        let horizontal = NSLayoutConstraint(item: view,
+                                            attribute: .centerX,
+                                            relatedBy: .equal,
+                                            toItem: self.customView,
+                                            attribute: .centerX,
+                                            multiplier: 1,
+                                            constant: 0)
+        
+        let vertical = NSLayoutConstraint(item: view,
+                                          attribute: .centerY,
+                                          relatedBy: .equal,
+                                          toItem: self.customView,
+                                          attribute: .centerY,
+                                          multiplier: 1,
+                                          constant: 0)
+        
+        let width = NSLayoutConstraint(item: view,
+                                       attribute: .width,
+                                       relatedBy: .equal,
+                                       toItem: nil,
+                                       attribute: .notAnAttribute,
+                                       multiplier: 1,
+                                       constant: width)
+        
+        let height = NSLayoutConstraint(item: view,
+                                        attribute: .height,
+                                        relatedBy: .equal,
+                                        toItem: nil,
+                                        attribute: .notAnAttribute,
+                                        multiplier: 1,
+                                        constant: height)
+        
+        self.customView.addConstraints([horizontal, vertical, width, height])
+    }
+    
 }
